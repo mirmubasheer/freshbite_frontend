7246d04f715f83889b59bbcddad37127420a06bf
Mir Mubasheer <fahadk558.mmk@gmail.com>
2024-02-24 13:07:31 +0530

    update again nextconfig.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,10 @@
 const nextConfig = {
   env: {
     API_URL: "https://multikart-graphql-reactpixelstrap.vercel.app/server.js",
+    NEXT_PUBLIC_GRAPHQL_API_ENDPOINT:
+      "https://freshbitegrocers-p89s8vze7-mubasheers-projects.vercel.app/graphql",
+    // Add more environment variables here if needed
   },
-  // env: {
-  //   next_public_graphql_api_endpoint:
-  //     "https://freshbitegrocers-p89s8vze7-mubasheers-projects.vercel.app/graphql",
-  // },
-
   // if you want to run with local graphQl un-comment below one and comment the above code
   // env: {
   //   API_URL: "http://localhost:4000/graphql",
